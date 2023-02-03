f21c873629a6ae072df9fe26202ea2861617fddf
Stas Ivanov <toster.one@yandex.ru>
2023-02-03 21:29:39 +0300

    we started work with arrays

--- ArrayProject.cpp
+++ ArrayProject.cpp
@@ -2,5 +2,13 @@
 using namespace std;
 int main()
 {
-    std::cout << "Hello World!\n";
+    //short mas[4] = {10,20,30,40};
+    //mas[0] = 3000;
+    //cout << mas[0];
+    int mas[10] = {};
+    for (int i = 0; i <= 9; i++) {
+        mas[i] = i+1;
+        cout << mas[i]<<endl;
+    }
+ 
 }
